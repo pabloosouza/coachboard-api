a250f3cd60e0cf497075dc43d0f3492b069142bc
Pablo Souza <soupabloliveira@gmail.com>
2024-01-26 11:03:31 -0300

    feat: linking controllers to presentation layer

--- src/Presentation/DependencyInjection.cs
+++ src/Presentation/DependencyInjection.cs
@@ -1,6 +1,15 @@
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Presentation;
 
-public class DependencyInjection
+public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        services.AddControllers()
+            .AddApplicationPart(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
 }
